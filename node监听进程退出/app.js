22863688d53620e0e1ebe614bdecfb5cb13ae762
hrero <279768876@qq.com>
2022-11-12 16:15:18 +0800

    a

---
+++ node监听进程退出/app.js
@@ -0,0 +1,34 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end();
+});
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+server.listen(8000);
+process.on('SIGTERM', close.bind(this, 'SIGTERM'));
+process.on('SIGINT', close.bind(this, 'SIGINT'));
+process.on('SIGKILL', close.bind(this, 'SIGKILL'));
+
+function close(signal) {
+    console.log(`收到 ${signal} 信号开始处理`);
+
+    server.close(() => {
+        console.log(`服务停止 ${signal} 处理完毕`);
+        process.exit(0);
+    });
+}
+// process.on("SIGTERM", () => {
+//     console.log(1);
+//     process.exit(1)
+// })
+// process.on('exit', (code) => {
+//     console.log(`即将退出，退出码：${code}`);
+// });
+// process.stdin.resume();
+
+// process.on('SIGINT', () => {
+//   console.log('Received SIGINT.  Press Control-D to exit.');
+// });
+// process.exit()
